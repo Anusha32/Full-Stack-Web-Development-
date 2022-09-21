efcc88409883b0ec386736f34431d5008e126387
Anusha32 <anushabr3@gmail.com>
2022-09-21 17:09:10 +0530

    c

--- practiceapp/src/index.js
+++ practiceapp/src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import  ReactDOM from 'react-dom';
+import React from "react";
+import  ReactDOM  from "react-dom";
 
-class Books extends React.Component{
-  render(){
-    return <h1>This is the collection of all the books I have read.</h1>
-  }
+// class Books extends React.Component {
+
+//   render() {
+//     return <h1>This is the collection of all the books I have read.</h1>
+//   }
+// }
+
+// ReactDOM.render(<Books />,
+//  document.getElementById('root'));
+
+//function component
+
+function Welcome(props){
+  return <h1>Hello,</h1>
 }
-
-ReactDOM.render(<Books />,
-  document.getElementById('root'));
